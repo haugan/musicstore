02a29c4038c2a837063fa0abbbf172d182e4d3e3
Marius Riis Haugan <hauganmarius@outlook.com>
2018-05-14 08:12:28 +0200

    Initial repository interfaces

---
+++ MusicStore/Models/Repositories/IAlbumRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicStore.Models.Repositories
+{
+    public interface IAlbumRepository
+    {
+        IQueryable<Album> Albums { get; }
+    }
+}
